0255d406804c8abe465193505db1597b7c9f6ad2
Natalia Lysyshyn <natalialysyshyn@Natalias-MBP.attlocal.net>
2024-07-22 13:15:16 -0500

    first commit

---
+++ src/main/java/GITClass.java
@@ -0,0 +1,17 @@
+public class GITClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello GIT");
+        System.out.println("Git add");
+
+
+
+
+
+
+
+
+
+    }
+}
